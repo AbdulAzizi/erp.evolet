a73ba44e26af0253ccbd07c5e66c3b0404f17232
abdulAziz <abdulaziz@1921684226.local>
2019-04-30 14:54:32 +0500

    Add Admin seeder

---
+++ database/seeds/TableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = App\User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
