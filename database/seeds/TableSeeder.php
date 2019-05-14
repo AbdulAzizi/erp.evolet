19b4fe7ccab838a5cfbde29ca267979a11171ece
abdulAziz <abdulaziz@1921684226.local>
2019-05-14 10:23:34 +0500

    Add Field Seeder

--- database/seeds/TableSeeder.php
+++ database/seeds/TableSeeder.php
@@ -11,6 +11,22 @@ class TableSeeder extends Seeder
      */
     public function run()
     {
-        
+        $mnn = App\Field::create(['name' => 'МНН', 'slug' => 'mnn']);
+        $form = App\Field::create(['name' => 'Форма', 'slug' => 'form']);
+        $doza = App\Field::create(['name' => 'Доза', 'slug' => 'doza']);
+        $opu = App\Field::create(['name' => 'ОПУ', 'slug' => 'opu']);
+        $thchp = App\Field::create(['name' => 'ТХЧП', 'slug' => 'thchp']);
+        $perviy_god = App\Field::create(['name' => 'Первый год', 'slug' => 'perviy_god']);
+        $summa_prodazh_za_perviy_god = App\Field::create(['name' => 'Сумма продаж за первый год', 'slug' => 'summa_prodazh_za_perviy_god']);
+        $prodazhi_upakovok_za_perviy_god = App\Field::create(['name' => 'Продажы упаковок за первый год', 'slug' => 'prodazhi_upakovok_za_perviy_god']);
+        $vtoroi_god = App\Field::create(['name' => 'Второй год', 'slug' => 'vtoroi_god']);
+        $summa_prodazh_za_vtoroi_god = App\Field::create(['name' => 'Сумма продаж за второй год', 'slug' => 'summa_prodazh_za_vtoroi_god']);
+        $prodazhi_upakovok_za_vtoroi_god = App\Field::create(['name' => 'Продажы упаковок за второй год', 'slug' => 'prodazhi_upakovok_za_vtoroi_god']);
+        $kppr = App\Field::create(['name' => 'КППР', 'slug' => 'kppr']);
+        $dolya_bg = App\Field::create(['name' => 'Доля БГ', 'slug' => 'dolya_bg']);
+        $dolya_mst = App\Field::create(['name' => 'Доля Мст', 'slug' => 'dolya_mst']);
+        $prirost_mst = App\Field::create(['name' => 'Прир Мст', 'slug' => 'prirost_mst']);
+        $nkpn = App\Field::create(['name' => 'НКПН', 'slug' => 'nkpn']);
+        $nkpf = App\Field::create(['name' => 'НКПФ', 'slug' => 'nkpf']);
     }
 }
