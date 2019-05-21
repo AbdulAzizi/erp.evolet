c3ffb6afc53845e225f14d4497cf7cb53370dc9a
Ergashev Akbar <ergashev.akb@gmail.com>
2019-05-21 14:58:24 +0500

    Remove employee data fetching

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -8,14 +8,6 @@ class EmployeeController extends Controller
 {
     public function show()
     {
-        $employee = Employee::where('user_id', auth()->id())
-            ->with('user')
-            ->with('division:id,name,abbreviation')
-            ->with('position')
-            ->with('responsibility')
-            ->first();
-
-        // dd($employee);
-        return view('profile', compact('employee'));
+        return view('profile');
     }
 }
