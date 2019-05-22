71c66d6bcd1a4b4f34529176c4fca0e2c49fa8dd
Ergashev Akbar <ergashev.akb@gmail.com>
2019-05-22 16:07:09 +0500

    Refactor users factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,13 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $surname = $faker->unique()->lastName;
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => $faker->firstName,
+        'surname' => $surname,
+        'email' =>  $surname . '@admin.com',
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('admin'),
         'remember_token' => Str::random(10),
     ];
 });
