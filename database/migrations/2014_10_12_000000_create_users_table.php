6e4d9bd87e1c9069f3a4338abf1b6c1e657469fa
Ergashev Akbar <ergashev.akb@gmail.com>
2019-05-23 14:31:32 +0500

    Add default user photo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('surname');
-            $table->string('img')->nullable();
+            $table->string('img')->nullable()->default('default-user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
