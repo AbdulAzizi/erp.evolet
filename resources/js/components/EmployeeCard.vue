642de7ed3aa520d6627e621ffad03f5a0a782d84
Ergashev Akbar <ergashev.akb@gmail.com>
2019-05-24 15:37:25 +0500

    Finish first version of employee card

--- resources/js/components/EmployeeCard.vue
+++ resources/js/components/EmployeeCard.vue
@@ -1,11 +1,13 @@
 <template>
+
   <v-card class="text-xs-center pa-3">
     <v-avatar size="140">
-      <v-img :src="`../img/${employee.img}.jpg`"/>
+      <v-img :src="`../img/${employee.user.img}.jpg`"  />
     </v-avatar>
 	
-    <h5 class="subheading text-xs-center">{{employee.position.name}}</h5>
-    <h5 class="subheading text-xs-center pa-3 ">{{`${employee.name} ${employee.surname}`}}</h5>
+    <h6 class="subheading mt-2 grey--text">{{employee.position.name}}</h6>
+    <h6 class="title py-1 primary--text" >{{`${employee.user.name} ${employee.user.surname}`}}</h6>
+    <h6 class="body-2 grey--text ">{{employee.responsibility.name}}</h6>
 
   </v-card>
 </template>
