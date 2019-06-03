a7d645a135aaf8693d0e6faad9c146c456cb29be
Abdulaziz Nurov <nurovaziz@gmail.com>
2019-06-03 16:17:24 +0500

    Add Priority component

---
+++ resources/js/components/Priority.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-tooltip top>
+        <template v-slot:activator="{ on:tooltip }">
+            <v-icon v-on="{ ...tooltip }" :color="priorities[id].color">flag</v-icon>
+        </template>
+        <span>{{priorities[id].label}} Приоритет</span>
+    </v-tooltip>
+</template>
+
+<script>
+export default {
+    props:['id'],
+    data(){
+        return {
+            priorities:[
+                { id:0, label:"Низкий", color:"light-blue lighten-4" },
+                { id:1, label:"Средний", color:"amber lighten-1" },
+                { id:2, label:"Высокий", color:"red" }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
