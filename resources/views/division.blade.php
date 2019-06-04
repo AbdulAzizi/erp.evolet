09eab7a2f53609afb8a8357465487256564be6a0
Ergashev Akbar <ergashev.akb@gmail.com>
2019-06-04 17:22:28 +0500

    Refactor division page

--- resources/views/division.blade.php
+++ resources/views/division.blade.php
@@ -3,26 +3,25 @@
 @section('content')
 
 @php
-    $isEmployeeHead = $division[0]->head->id === $authUser->employee->id;
+    // dd($division->toArray());
+    $isEmployeeHead = false;
+
+    if($division[0]->head){
+        $isEmployeeHead = $division[0]->head->id === $authUser->employee->id;
+    }
     $isEmployeeHead = json_encode($isEmployeeHead);
     
 @endphp
-
-{{-- {{dd($positions->toArray())}} --}}
 
 <v-container fluid ma-0 grid-list-lg>
     <v-layout>
         <v-flex xs10>
             <division :division="{{$division[0]}}" :is-employee-head="{{$isEmployeeHead}}" />
-        </v-flex>
-    </v-layout>
+</v-flex>
+</v-layout>
 </v-container>
 
-<add-employee-dialog 
-    :responsibilities="{{$responsibilities}}" 
-    :positions="{{$positions}}" 
-    :errors="{{$errors}}"
-    :oldinputs="{{json_encode(Session::getOldInput())}}"
-/>
+<add-employee-dialog :responsibilities="{{$responsibilities}}" :positions="{{$positions}}" :errors="{{$errors}}"
+    :oldinputs="{{json_encode(Session::getOldInput())}}" />
 
 @endsection
