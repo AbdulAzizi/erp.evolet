c135727eed13bb89dcdfa04181d37053dcf8542e
Abdulaziz Nurov <nurovaziz@gmail.com>
2019-06-11 16:15:14 +0500

    Make new component Avatar Sets

--- resources/js/app.js
+++ resources/js/app.js
@@ -82,6 +82,7 @@ Vue.component("tasks-watchers", require("./components/tasks/Watchers.vue").defau
 Vue.component("employee-card", require("./components/EmployeeCard.vue").default);
 Vue.component("division", require("./components/Division.vue").default);
 Vue.component("add-employee-dialog", require("./components/AddEmployeeDialog.vue").default);
+Vue.component("avatars-set", require("./components/AvatarsSet.vue").default);
 
 Vue.component(
     "user-selector",
---
+++ resources/js/components/AvatarsSet.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <v-tooltip 
+        bottom
+        v-for="(item,index) in items"
+        :key="'avatar-'+index"
+        >
+            <template v-slot:activator="{ on }">
+                <a v-on="on" :href="appPath+'employees/'+item.id" class="avatar-item" >
+                    <v-avatar
+                    color="grey lighten-4"
+                    size="40"
+                    class="elevation-2 "
+                    :style=" {'z-index': (items.length - index)} "
+                    >
+                        <img :src="photo(item.user.img)"  alt="avatar">
+                    </v-avatar>
+                </a>
+            </template>
+            <span>{{item.user.name}} {{item.user.surname}} ({{item.label}})</span>
+        </v-tooltip>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['watchers','assignee','from', 'label'],
+    data(){
+        return{
+            appPath:window.Laravel.asset_path
+        }
+    },
+    computed:{
+        items(){
+            let items = this.watchers.map( function(item){
+                item['label'] = 'Наблюдатель';
+                return item; 
+            });
+
+            this.assignee['label'] = 'Исполнитель';
+            this.from['label'] = 'Постановщик';
+
+            items.push(this.assignee,this.from);
+            console.log(items);
+
+            return items;
+        }
+           
+    }
+}
+</script>
+
+<style>
+.avatar-item{
+    margin-left:-20px;
+    display: inline-block;
+    -webkit-transition: all 0.3s; /* Safari prior 6.1 */
+    transition: all 0.3s;
+}
+.avatar-item:first-child{
+    margin-left:0px;
+}
+
+.avatar-item:hover .v-avatar{
+    z-index: 99999 !important;
+}
+.avatar-item:hover{
+    margin-left:-10px;
+    margin-right: 10px;
+}
+.avatar-item:first-child:hover{
+    margin-left:0px;
+    margin-right: 0px;
+}
+
+
+</style>
