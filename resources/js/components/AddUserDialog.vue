b95b61834a9515c4f6971964ae0da5dcaeee05cf
Ergashev Akbar <ergashev.akb@gmail.com>
2019-06-24 10:40:03 +0500

    Refactor user adding form to use dynamic form fields

--- resources/js/components/AddUserDialog.vue
+++ resources/js/components/AddUserDialog.vue
@@ -1,111 +1,80 @@
 <template>
-  <v-dialog v-model="show" max-width="600">
-    <v-form action="users" method="POST">
-      <input type="hidden" name="_token" :value="csrf">
-      <v-card>
-        <v-toolbar flat color="primary" dark>
-          <v-toolbar-title class="font-weight-regular">Новый сотрудник</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-text-field
-            v-model="name"
-            name="name"
-            label="Имя"
-            required
-            :error-messages="localErrors.name"
-          />
-          <v-text-field
-            v-model="surname"
-            name="surname"
-            label="Фамилия"
-            required
-            :error-messages="localErrors.surname"
-          />
-          <v-text-field
-            v-model="email"
-            name="email"
-            label="Email"
-            required
-            :error-messages="localErrors.email"
-          />
-          <v-select
-            v-model="positionId"
-            :items="positions"
-            item-text="name"
-            item-value="id"
-            label="Должность"
-            :error-messages="localErrors.positionId"
-          />
-          <input type="hidden" name="positionId" :value="positionId">
-          <v-select
-            v-model="responsibilityId"
-            :items="responsibilities"
-            item-text="name"
-            item-value="id"
-            label="Полномочия"
-            :error-messages="localErrors.responsibilityId"
-          />
-          <input type="hidden" name="responsibilityId" :value="responsibilityId">
+    <v-dialog v-model="show" max-width="600">
+        <v-form action="users" method="POST">
+            <v-card>
+                <v-toolbar flat color="primary" dark>
+                    <v-toolbar-title class="font-weight-regular">Новый сотрудник</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
 
-          <input type="hidden" name="divisionId" :value="divisionId">
-        </v-card-text>
+                    <form-field v-for="(field, i) in formFields" :key="i" :field="field" :errors="errors" :old-inputs="oldinputs"/>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" flat="flat" @click="show = false">Отмена</v-btn>
-          <v-btn color="primary" type="submit">Добавить</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-form>
-  </v-dialog>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" flat="flat" @click="show = false">Отмена</v-btn>
+                    <v-btn color="primary" type="submit">Добавить</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-form>
+    </v-dialog>
 </template>
 
 <script>
 //TODO use same design as in tasks/Add
 export default {
-  props: ["positions", "responsibilities", "errors", "oldinputs"],
-  data() {
-    return {
-      localErrors: this.errors,
-      localOldInputs: this.oldinputs,
+    props: ["positions", "responsibilities", "errors", "oldinputs"],
+    data() {
+        return {
+        
+            show: Array.isArray(this.errors) ? false : true,
+            csrf: window.Laravel.csrf_token,
 
-      show: Array.isArray(this.errors) ? false : true,
-      csrf: window.Laravel.csrf_token,
+            divisionId: null
+        };
+    },
+    created() {
 
-      name: "",
-      surname: "",
-      email: "",
-      positionId: "",
-      responsibilityId: "",
-      divisionId: ''
-    };
-  },
-  created() {
-    
-    const { name, surname, email, positionId, responsibilityId, divisionId } = this.localOldInputs;
-
-    this.name = name;
-    this.surname = surname;
-    this.email = email;
-    this.positionId = parseInt(positionId);
-    this.responsibilityId = parseInt(responsibilityId);
-    this.divisionId = divisionId;
-
-    Event.listen("addUser", data => {
-      this.localErrors = [];
-      this.localOldInputs = null;
-
-      this.show = true;
-      this.name = "";
-      this.surname = "";
-      this.email = "";
-      this.positionId = null;
-      this.responsibilityId = null;
-
-      this.divisionId = data.divisionId;
-    });
-
-  }
+        Event.listen("addUser", data => {
+            this.show = true;
+            this.divisionId = data.divisionId;
+        });
+    },
+    computed: {
+        formFields() {
+            return [
+                { type: "input", name: "_token", value: this.csrf },
+                { type: "input", name: "divisionId", value: this.divisionId },
+                {
+                    type: "string",
+                    name: "name",
+                    label: "Имя",
+                },
+                {
+                    type: "string",
+                    name: "surname",
+                    label: "Фамилия",
+                },
+                {
+                    type: "string",
+                    name: "email",
+                    label: "Email",
+                },
+                {
+                    type: "select",
+                    name: "positionId",
+                    label: "Должность",
+                    items: this.positions
+                },
+                {
+                    type: "autocomplete",
+                    name: "responsibilityId",
+                    label: "Полномочия",
+                    items: this.responsibilities,
+                }
+            ];
+        }
+    }
 };
 </script>
 
