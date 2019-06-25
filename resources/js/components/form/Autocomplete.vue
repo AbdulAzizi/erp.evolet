1248f338162234e103b27f9011bdfbcf2853803d
Ergashev Akbar <ergashev.akb@gmail.com>
2019-06-25 17:49:36 +0500

    For custom chips styling make separate autocomplete component

---
+++ resources/js/components/form/Autocomplete.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-autocomplete
+        v-model="selectedItems"
+        :items="items"
+        :item-text="itemText"
+        :item-value="itemValue"
+        hide-selected
+        :label="label"
+        v-bind="$attrs"
+    >
+        <template v-slot:selection="{ item, parent, selected }">
+            <v-chip color="primary" :selected="selected" dark small>
+                <span class="pr-1">{{ item[itemText] }}</span>
+                <v-icon small @click="parent.selectItem(item)">close</v-icon>
+            </v-chip>
+        </template>
+    </v-autocomplete>
+</template>
+
+<script>
+export default {
+    props: ["name", "label", "items", "itemText", "itemValue"], //All other props goes from form-field
+    data() {
+        return {
+            selectedItems: []
+        };
+    },
+    watch: {
+        selectedItems(value) {
+            this.$emit("input", value);
+        }
+    }
+};
+</script>
