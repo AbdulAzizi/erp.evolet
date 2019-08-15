d25b6705305615b12febf0911b0791537e0f9dff
Asliddin <bum9696@gmail.com>
2019-08-15 17:28:37 +0500

    New fullWidth layout without cntainer

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.default')
+@extends('layouts.fullWidth')
 
 @section('content')
     @php
         $jsonForm = isset($form) ? $form : json_encode(null) ;
     @endphp
-        <products-view 
-        :form="{{$jsonForm}}" 
+        <products-view
+        :form="{{$jsonForm}}"
         :items="{{$products}}"
         :participants="{{$participants}}"
         ></products-view>
