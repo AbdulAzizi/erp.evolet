0acf51ed1f489b2ff8e3b8829f939bf2a9a42de8
bakhodur-nazriev96 <bakhodur.nazriev@gmail.com>
2019-08-16 19:30:09 +0500

    Add active style to toggle button

--- resources/js/components/views/Tasks.vue
+++ resources/js/components/views/Tasks.vue
@@ -2,25 +2,28 @@
     <v-card-text fluid  class="pa-0">
       <v-card-text class="pt-0 px-0">
         <v-layout justify-end>
-          <v-btn-toggle v-model="text">
+          <v-btn-toggle v-model="text" active-class="primary">
             <v-btn
               text
               value="table"
-              color="primary"
-              @click="selectTab(1)"
-              ><v-icon>mdi-table-large</v-icon></v-btn>
+              @click="isTable = true"
+              >
+                <v-icon :color="isTable ? 'white': 'grey lighten-0'">mdi-table-large</v-icon>
+              </v-btn>
             <v-btn
               text
               value="calendar"
               color="primary"
-              @click="selectTab(2)"
-            ><v-icon>mdi-calendar-month</v-icon></v-btn>
+              @click="isTable = false"
+            >
+              <v-icon :color="!isTable ? 'white': 'grey lighten-0'">mdi-calendar-month</v-icon>
+            </v-btn>
           </v-btn-toggle>
         </v-layout>
       </v-card-text>
 
-      <tasks-table :tasks="tasks" v-if="currentTab == 1"></tasks-table>
-      <tasks-calendar :tasks="tasks" v-if="currentTab == 2"></tasks-calendar>
+      <tasks-table :tasks="tasks" v-if="isTable"></tasks-table>
+      <tasks-calendar :tasks="tasks" v-if="!isTable"></tasks-calendar>
 
       <tasks-add :users="users" :errors="errors" :tags="tags" />
     </v-card-text>
@@ -34,13 +37,7 @@ export default {
     return {
       text: "table",
       icon: "justify",
-      toggle_none: null,
-      toggle_one: 0,
-      toggle_exclusive: 2,
-      toggle_multiple: [0, 1, 2],
-      // firstSwitcher: true,
-      // secondSwitcher: false,
-      currentTab: 1,
+      isTable: true,
       justify: "end"
     };
   },
@@ -53,7 +50,5 @@ export default {
 </script>
 
 <style>
-.block_toggle_btn {
-  padding: 0;
-}
+
 </style>
