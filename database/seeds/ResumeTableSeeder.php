5727fad519c7630da83a36e0c39662aeccad2b17
Asliddin <bum9696@gmail.com>
2019-08-20 10:46:09 +0500

    Take yser by email

--- database/seeds/ResumeTableSeeder.php
+++ database/seeds/ResumeTableSeeder.php
@@ -25,7 +25,7 @@ class ResumeTableSeeder extends Seeder
             'birthday' => new Carbon('1996-01-17'),
             'military_status' => 'Не объязан',
             'phone' => '985100888',
-            'user_id' => User::where('name', 'AbdulAziz')->first()->id,
+            'user_id' => User::where('email', 'nurovaziz@gmail.com')->first()->id,
 
         ]);
 
@@ -37,7 +37,7 @@ class ResumeTableSeeder extends Seeder
             'end_at' => new Carbon('2018-06-20'),
             'name' => 'Misis',
             'specialty' => 'Computer Science',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -48,7 +48,7 @@ class ResumeTableSeeder extends Seeder
             'end_at' => new Carbon('2020-06-20'),
             'name' => 'MGU',
             'specialty' => 'Computer Science',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -59,7 +59,7 @@ class ResumeTableSeeder extends Seeder
             'end_at' => new Carbon('2024-06-20'),
             'name' => 'Bauman',
             'specialty' => 'Computer Science',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -71,7 +71,7 @@ class ResumeTableSeeder extends Seeder
             'company_name' => 'Alif',
             'position' => 'Dev',
             'location' => 'Dushanbe',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -81,7 +81,7 @@ class ResumeTableSeeder extends Seeder
             'relation' => 'Жена',
             'birthday' => new Carbon('1997-08-04'),
             'name' => 'Евгения',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -90,7 +90,7 @@ class ResumeTableSeeder extends Seeder
             'relation' => 'Сын',
             'birthday' => new Carbon('2000-09-21'),
             'name' => 'Джумахон',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -99,7 +99,7 @@ class ResumeTableSeeder extends Seeder
             'relation' => 'Дочь',
             'birthday' => new Carbon('2002-10-28'),
             'name' => 'Лола',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -108,7 +108,7 @@ class ResumeTableSeeder extends Seeder
 
             'name' => 'Английский',
             'level' => 'Intermidiate',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -116,7 +116,7 @@ class ResumeTableSeeder extends Seeder
 
             'name' => 'Немецкий',
             'level' => 'Pre-Intermidiate',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -124,7 +124,7 @@ class ResumeTableSeeder extends Seeder
 
             'name' => 'Русский',
             'level' => 'За школой',
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -137,7 +137,7 @@ class ResumeTableSeeder extends Seeder
                              Nemo natus voluptatem accusamus recusandae aliquid maxime.
                              Commodi ex distinctio perspiciatis.',
 
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
 
@@ -150,7 +150,7 @@ class ResumeTableSeeder extends Seeder
                              Nemo natus voluptatem accusamus recusandae aliquid maxime.
                              Commodi ex distinctio perspiciatis.',
 
-            'resume_id' => $abdulazizResume->first()->id
+            'resume_id' => $abdulazizResume->id
 
         ]);
     }
