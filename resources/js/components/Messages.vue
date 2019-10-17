ec0feca073d1ed1d3ee40841a44e37f4546df277
UnknownCoolGuy <bum9696@gmail.com>
2019-10-17 11:52:02 +0500

    Display date of message in chat

--- resources/js/components/Messages.vue
+++ resources/js/components/Messages.vue
@@ -1,131 +1,155 @@
 <template>
-    <v-container fluid class="py-0 grey lighten-3" >
-        <v-row style="height:calc(100vh - 230px); overflow:auto;" ref="messagesWrapper">
-            <v-container fluid>
-                <v-row
-                    v-for="(message,index) in localMessageable.messages"
-                    :key="'message-'+index"
-                    :justify="(( message.sender.id === auth.id ) ? 'end' : 'start')"
-                >
-                    <avatar
-                        :user="message.sender"
-                        size="30"
-                        :class="' ml-2 mt-2'"
-                        v-if="message.sender.id != auth.id"
-                    />
+  <v-container fluid class="py-0 grey lighten-3">
+    <v-row style="height:calc(100vh - 230px); overflow:auto;" ref="messagesWrapper">
+      <v-container fluid>
+        <v-row
+          v-for="(message,index) in localMessageable.messages"
+          :key="'message-'+index"
+          :justify="(( message.sender.id === auth.id ) ? 'end' : 'start')"
+        >
+          <avatar
+            :user="message.sender"
+            size="30"
+            :class="' ml-2 mt-2'"
+            v-if="message.sender.id != auth.id"
+          />
+          <v-card flat :class="'mx-2 mt-2'" max-width="60%">
+            <v-card-text class="pa-2">
+              <p class="ma-0">{{message.body}}</p>
+              <p
+                class="ma-0 text-lowercase grey--text float-right"
+                style="font-size: 10px"
+              >{{showDateOfMessage(message.created_at)}}</p>
+            </v-card-text>
+          </v-card>
 
-                    <v-card flat :class="'mx-2 mt-2'" max-width="60%">
-                        <v-card-text class="pa-2">{{message.body}}</v-card-text>
-                    </v-card>
-
-                    <avatar
-                        :user="message.sender"
-                        size="30"
-                        :class="' mr-2 mt-2'"
-                        v-if="message.sender.id === auth.id"
-                    />
-                </v-row>
-            </v-container>
+          <avatar
+            :user="message.sender"
+            size="30"
+            :class="' mr-2 mt-2'"
+            v-if="message.sender.id === auth.id"
+          />
         </v-row>
-        <v-row>
-            <v-col cols="12 pa-0" align-self="end">
-                <v-card tile flat>
-                    <v-text-field
-                        v-model="body"
-                        hide-details
-                        append-icon="mdi-send"
-                        solo
-                        @click:append="body ? storeMessage(localMessageable) : '' "
-                        @keyup.enter="body ? storeMessage(localMessageable) : '' "
-                    ></v-text-field>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+      </v-container>
+    </v-row>
+    <v-row>
+      <v-col cols="12 pa-0" align-self="end">
+        <v-card tile flat>
+          <v-text-field
+            v-model="body"
+            hide-details
+            append-icon="mdi-send"
+            solo
+            @click:append="body ? storeMessage(localMessageable) : '' "
+            @keyup.enter="body ? storeMessage(localMessageable) : '' "
+          ></v-text-field>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
-    props: {
-        messageable: {
-            required: true
-        },
-        type: {
-            required: true
-        }
+  props: {
+    messageable: {
+      required: true
     },
-    data() {
-        return {
-            body: "",
-            localMessageable: this.messageable
-        };
-    },
-    created() {
-        // console.log('------------------');
-        
-        // console.log(this.messageable);
-        // console.log(this.messageable.type);
-        
-        switch (this.type) {
-            case 'App\\User':
-                Echo.channel(`newMessage.Users.${this.auth.id}.${this.messageable.id}`).listen("NewMessage", event => {
-                    // console.log(event.message);
-                    this.localMessageable.messages.push(event.message);
-                    this.scrollToBotom();
-                });
+    type: {
+      required: true
+    }
+  },
+  data() {
+    return {
+      body: "",
+      localMessageable: this.messageable
+    };
+  },
+  created() {
+    // console.log('------------------');
 
-                Echo.channel(`newMessage.Users.${this.messageable.id}.${this.auth.id}`).listen("NewMessage", event => {
-                    // console.log(event.message);
-                    this.localMessageable.messages.push(event.message);
-                    this.scrollToBotom();
-                });
+    // console.log(this.messageable);
+    // console.log(this.messageable.type);
 
-                break;
-        
-            default:
+    switch (this.type) {
+      case "App\\User":
+        Echo.channel(
+          `newMessage.Users.${this.auth.id}.${this.messageable.id}`
+        ).listen("NewMessage", event => {
+          // console.log(event.message);
+          this.localMessageable.messages.push(event.message);
+          this.scrollToBotom();
+        });
 
-                Echo.channel(`newMessage.Chats.${this.messageable.id}`).listen("NewMessage", event => {
-                    // console.log(event.message);
-                    this.localMessageable.messages.push(event.message);
-                    this.scrollToBotom();
-                });
+        Echo.channel(
+          `newMessage.Users.${this.messageable.id}.${this.auth.id}`
+        ).listen("NewMessage", event => {
+          // console.log(event.message);
+          this.localMessageable.messages.push(event.message);
+          this.scrollToBotom();
+        });
 
-                break;
-        }
-        
-    },
-    methods: {
-        storeMessage(messageable) {
-            let self = this;
-            axios
-                .post(this.appPath("api/messages"), {
-                    body: self.body,
-                    messageable_id: messageable.id,
-                    messageable_type: self.type
-                })
-                .then(function(response) {
-                    console.log(response);
-                    
-                    self.body = "";
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
-        },
-        scrollToBotom() {
-            this.$nextTick(function() {
-                this.$refs.messagesWrapper.scrollTop =
-                    this.$refs.messagesWrapper.scrollHeight;
-            });
-        }
-    },
-    watch: {
-        messageable(val) {
-            console.log("commantable");
-            console.log(this.messageable);
-            
-            this.localMessageable = this.messageable;
+        break;
+
+      default:
+        Echo.channel(`newMessage.Chats.${this.messageable.id}`).listen(
+          "NewMessage",
+          event => {
+            // console.log(event.message);
+            this.localMessageable.messages.push(event.message);
             this.scrollToBotom();
+          }
+        );
+
+        break;
+    }
+  },
+  methods: {
+    storeMessage(messageable) {
+      let self = this;
+      axios
+        .post(this.appPath("api/messages"), {
+          body: self.body,
+          messageable_id: messageable.id,
+          messageable_type: self.type
+        })
+        .then(function(response) {
+          console.log(response);
+
+          self.body = "";
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    scrollToBotom() {
+      this.$nextTick(function() {
+        this.$refs.messagesWrapper.scrollTop = this.$refs.messagesWrapper.scrollHeight;
+      });
+    },
+    showDateOfMessage(date){
+        let currentYear = this.moment().local().format('Y');
+        let currentDay = this.moment().local().format('DD');
+        let messageDate = this.moment(date).local();
+
+        if(messageDate.format('Y') !== currentYear){
+            return messageDate.format('hh:mm DD.M.YY');
+        }
+        if(messageDate.format('DD') !== currentDay){
+            return messageDate.format('DD MMM hh:mm');
+        }
+        else{
+            return messageDate.format('hh:mm');
         }
     }
+  },
+  watch: {
+    messageable(val) {
+      console.log("commantable");
+      console.log(this.messageable);
+
+      this.localMessageable = this.messageable;
+      this.scrollToBotom();
+    }
+  }
 };
 </script>
