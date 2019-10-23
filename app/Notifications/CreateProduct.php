ead36c9812154099843cb431a58402ce6f9fbfa4
UnknownCoolGuy <bum9696@gmail.com>
2019-10-23 10:30:50 +0500

    Create product create observer

---
+++ app/Notifications/CreateProduct.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+
+class CreateProduct extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($from, $product, $project)
+    {
+        $this->from = $from;
+        $this->product = $product;
+        $this->project = $project;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database', 'broadcast'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        $url = 'products?project_id=' . $this->project->id;
+        $country = $this->project->country->name;
+        $pc = $this->project->pc->name;
+
+        return [
+            'avatar' => $this->from->img,
+            'title' =>  '<a href="' . route("users.show", $this->from->id) . '">' . $this->from->name . ' ' . $this->from->surname . '</a>' .
+                ' добавил(a) <a href="' . route("product.show", $this->product->id) . '">' . 'новый продукт' .
+            '</a>' . ' в проекте <a href="' . $url . '">' . $pc . ' · ' . $country . '</a>',
+        ];
+    }
+
+    public function toBroadcast($notifiable)
+    {
+
+        return new BroadcastMessage([
+            'avatar' => $this->from->img,
+            'title' =>  '<a href="' . route("users.show", $this->from->id) . '">' . $this->from->name . ' ' . $this->from->surname . '</a>' .
+               ' добавил(a) <a href="' . route("product.show", $this->product->id) . '">' . 'добавил(a) новый продукт' .
+                '</a>',
+            'notification' => $notifiable->notifications()->latest()->first()
+        ]);
+        // if procces
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,6 +7,8 @@ use App\Events\TaskCreatedEvent;
 use App\Events\TaskForwardedEvent;
 use App\Listeners\HistoryListener;
 use App\Observers\TaskObserver;
+use App\Observers\ProductObserver;
+use App\Product;
 use App\Task;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -44,5 +46,7 @@ class EventServiceProvider extends ServiceProvider
         parent::boot();
 
         Task::observe(TaskObserver::class);
+
+        Product::observe(ProductObserver::class);
     }
 }
