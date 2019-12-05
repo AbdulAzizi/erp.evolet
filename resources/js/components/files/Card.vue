e427ffd35aa7ce456a73855b397643c7cef95587
Abdulaziz Nurov <nurovaziz@gmail.com>
2019-12-05 14:22:30 +0500

    Make files frontend design

---
+++ resources/js/components/files/Card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <v-hover v-slot:default="{hover}">
+            <v-card @click="dialog=true" :elevation="hover ? 10 : 3">
+                <v-toolbar color="white" flat>
+                    <v-toolbar-title>File name</v-toolbar-title>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn icon v-if="hover">
+                        <v-icon>mdi-pencil</v-icon>
+                    </v-btn>
+
+                    <v-btn icon v-if="hover">
+                        <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                </v-toolbar>
+            </v-card>
+        </v-hover>
+        <v-dialog v-model="dialog" width="800px">
+            <v-card>
+                <v-card-title>File dialog with all its fields</v-card-title>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        item: {}
+    },
+    data() {
+        return {
+            dialog: false
+        };
+    }
+};
+</script>
---
+++ resources/js/components/files/Cards.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-col md="3" v-for="(file,index) in 20" :key="'file-'+index">
+                <file-card :item="file" />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+export default {
+    props: {
+        items: {}
+    }
+};
+</script>
