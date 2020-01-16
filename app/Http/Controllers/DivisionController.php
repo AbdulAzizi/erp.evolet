59a381072f7439014ca077fb2c53e1409fd287e9
Abdulaziz Nurov <nurovaziz@gmail.com>
2020-01-16 15:53:43 +0500

    Fix division isHead variable bug

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Division;
 use App\Position;
-use App\Responsibility;
 use App\User;
 use Illuminate\Http\Request;
 use Session;
@@ -13,19 +12,17 @@ use Validator;
 class DivisionController extends Controller
 {
     public function create(Request $request)
-    
     {
         $parentDivision = Division::where('name', 'Промо Компания')->get();
 
         return Division::create([
             'name' => $request->name,
-            'abbreviation' => $request->abbreviation
+            'abbreviation' => $request->abbreviation,
         ], $parentDivision->first());
     }
 
-    public function show(Request $request)
+    public function show()
     {
-
         $userDivisionId = auth()->user()->division_id;
         $division = Division::with('head', 'users', 'responsibilities.descriptions')->withDepth()->descendantsAndSelf($userDivisionId)->toTree()->first();
 
@@ -34,13 +31,14 @@ class DivisionController extends Controller
 
         if ($division->head) {
             $isUserHead = $division->head->id === $authUser->id;
+        } else {
+            $isUserHead = false;
         }
 
         $isUserHead = json_encode($isUserHead);
         $oldInputs = json_encode(Session::getOldInput());
         $jsonPositions = json_encode($positions);
         $jsonResponsibilities = json_encode($division->responsibilities);
-
 
         return view('division', compact('division', 'positions', 'isUserHead', 'oldInputs', 'jsonPositions', 'jsonResponsibilities'));
     }
@@ -49,7 +47,7 @@ class DivisionController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'parentDivisionId' => 'required'
+            'parentDivisionId' => 'required',
         ]);
 
         if ($validator->fails()) {
