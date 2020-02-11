b2391f41b2fb15d16869de21f33d01a29262251f
Abdulaziz Nurov <nurovaziz@gmail.com>
2020-02-11 12:07:39 +0500

    Improve user horizontal card

--- resources/js/components/profile/UserCardHorizontal.vue
+++ resources/js/components/profile/UserCardHorizontal.vue
@@ -1,27 +1,23 @@
 <template>
     <v-card :href=" link ? appPath('users/' + user.id) : null" :hover="link">
         <v-row class="pa-0 ma-0">
-            <v-col cols="3" class="pa-0">
-                <v-img style="border-radius:4px 0 0 4px" :src="photo(user.img)"></v-img>
-            </v-col>
-            <v-col cols="9" class="py-2">
-                <h4 class="primary--text">{{ user.name }} {{ user.surname }}</h4>
-                <span style="font-size:12px">{{ user.position_level.name }}</span>
-                <br />
-                <template v-if="user.division">
-                    <span style="font-size:12px">{{user.division.name}}</span>
-                    <br />
-                </template>
-                <span
-                    style="font-size: 12px;"
-                    class="grey--text"
-                    v-for="(position, key) in user.positions"
-                    :key="'position-'+key"
-                >
-                    {{position.label}}
-                    <span v-if="key != user.positions.length-1">&#183;</span>
-                </span>
-            </v-col>
+            <v-img max-width="85" class="mr-2" style="border-radius:4px 0 0 4px" :src="photo(user.img)"></v-img>
+            <div cols="9" class="pa-0">
+                <div class="primary--text">{{ user.name }} {{ user.surname }}</div>
+                <div style="font-size:12px">{{ user.position_level.name }}</div>
+                <div v-if="user.division" style="font-size:12px">{{user.division.name}}</div>
+                <div>
+                    <span
+                        style="font-size: 12px;"
+                        class="grey--text"
+                        v-for="(position, key) in user.positions"
+                        :key="'position-'+key"
+                    >
+                        {{position.label}}
+                        <span v-if="key != user.positions.length-1">&#183;</span>
+                    </span>
+                </div>
+            </div>
         </v-row>
     </v-card>
 </template>
