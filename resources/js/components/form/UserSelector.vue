138fca532067a1374773428464ed45aa7335d5c0
UnknownCoolGuy <bum9696@gmail.com>
2020-02-19 15:54:04 +0500

    fix: select users from division

--- resources/js/components/form/UserSelector.vue
+++ resources/js/components/form/UserSelector.vue
@@ -1,122 +1,122 @@
 <template>
-    <div>
-        <v-autocomplete
-            v-model="selectedUsers"
-            :items="users ? users : preparedDivisions"
-            item-text="fullname"
-            item-value="id"
-            no-data-text="Данные отсутствуют"
-            hide-selected
-            chips
-            color="primary"
-            :label="label"
-            :prepend-icon="icon"
-            :hint="hint"
-            persistent-hint
-            v-bind="$attrs"
+  <div>
+    <v-autocomplete
+      v-model="selectedUsers"
+      :items="preparedDivisions"
+      item-text="fullname"
+      item-value="id"
+      no-data-text="Данные отсутствуют"
+      hide-selected
+      chips
+      color="primary"
+      :label="label"
+      :prepend-icon="icon"
+      :hint="hint"
+      persistent-hint
+      v-bind="$attrs"
+    >
+      <template slot="selection" slot-scope="data">
+        <v-chip
+          :input-value="data.selected"
+          color="primary"
+          textColor="white"
+          pill
+          close
+          @click:close="remove(data.item)"
         >
-            <template slot="selection" slot-scope="data">
-                <v-chip
-                    :input-value="data.selected"
-                    color="primary"
-                    textColor="white"
-                    pill
-                    close
-                    @click:close="remove(data.item)"
-                >
-                    <v-avatar left>
-                        <img :src="photo(data.item.img)" />
-                    </v-avatar>
-                    {{ data.item.name }} {{data.item.surname}}
-                </v-chip>
-            </template>
+          <v-avatar left>
+            <img :src="photo(data.item.img)" />
+          </v-avatar>
+          {{ data.item.name }} {{data.item.surname}}
+        </v-chip>
+      </template>
 
-            <template slot="item" slot-scope="data">
-                <template v-if="'surname' in data.item">
-                    <v-list-item-avatar>
-                        <img v-if="data.item.img" :src="photo(data.item.img)" />
-                    </v-list-item-avatar>
+      <template slot="item" slot-scope="data">
+        <template v-if="'surname' in data.item">
+          <v-list-item-avatar>
+            <img v-if="data.item.img" :src="photo(data.item.img)" />
+          </v-list-item-avatar>
 
-                    <v-list-item-content>
-                        <v-list-item-title>{{data.item.name}} {{data.item.surname}}</v-list-item-title>
-                        <v-list-item-subtitle>
-                            <span
-                                v-for="(position,key) in data.item.positions"
-                                :key="'position-'+key"
-                            >
-                                {{position.label}}
-                                <span v-if="key != data.item.positions.length-1">|</span>
-                            </span>
-                            <!-- - {{data.item.division.abbreviation}} -->
-                        </v-list-item-subtitle>
-                    </v-list-item-content>
-                </template>
-                <v-list-item-content v-else @click.stop="handleDivisionSelection(data.item)">
-                    <v-list-item-title>{{data.item.name}}</v-list-item-title>
-                </v-list-item-content>
-            </template>
-        </v-autocomplete>
-        <input type="hidden" :name="name" :value="JSON.stringify(selectedUsers)" />
-    </div>
+          <v-list-item-content>
+            <v-list-item-title>{{data.item.name}} {{data.item.surname}}</v-list-item-title>
+            <v-list-item-subtitle>
+              <span v-for="(position,key) in data.item.positions" :key="'position-'+key">
+                {{position.label}}
+                <span v-if="key != data.item.positions.length-1">|</span>
+              </span>
+              <!-- - {{data.item.division.abbreviation}} -->
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </template>
+        <v-list-item-content v-else @click.stop="handleDivisionSelection(data.item)">
+          <v-list-item-title>{{data.item.name}}</v-list-item-title>
+        </v-list-item-content>
+      </template>
+    </v-autocomplete>
+    <input type="hidden" :name="name" :value="JSON.stringify(selectedUsers)" />
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        users: {
-            required: false,
-            type: Array
-        },
-        divisions: {
-            required: false
-        },
-        label: String,
-        icon: String,
-        name: String,
-        hint: String,
-        value: null
+  props: {
+    users: {
+      required: false,
+      type: Array
     },
-    data() {
-        return {
-            selectedUsers: this.value || [],
-            searchText: null
-        };
+    divisions: {
+      required: false
     },
-    methods: {
-        handleDivisionSelection(el) {
-            // this.selectedUsers.pop();
-            this.divisions.forEach(division => {
-                if (division.id == el.id) {
-                    this.selectedUsers.push(
-                        ...division.users.map(user => user.id)
-                    );
-                }
-            });
-        },
-        remove(user) {
-            this.selectedUsers = this.selectedUsers.filter(
-                userId => userId !== user.id
-            );
+    label: String,
+    icon: String,
+    name: String,
+    hint: String,
+    value: null
+  },
+  data() {
+    return {
+      selectedUsers: this.value || [],
+      searchText: null
+    };
+  },
+  methods: {
+    handleDivisionSelection(el) {
+      // this.selectedUsers.pop();
+      this.divisions.forEach(division => {
+        if (division.id == el.id) {
+          division.users.forEach(user => {
+              if(!this.selectedUsers.includes(user.id)){
+                  this.selectedUsers.push(user.id);
+              }
+          });
         }
+      });
     },
-    watch: {
-        selectedUsers(selectedUsersId) {
-            this.$emit("input", selectedUsersId);
-        }
-    },
-    computed: {
-        preparedDivisions() {
-            let divisionWithUsers = [];
-            this.divisions.forEach(division => {
-                divisionWithUsers.push(division);
-                if (division.users.length) {
-                    division.users.forEach(user => {
-                        divisionWithUsers.push(user);
-                    });
-                }
-            });
-            return divisionWithUsers;
-        }
+    remove(user) {
+      this.selectedUsers = this.selectedUsers.filter(
+        userId => userId !== user.id
+      );
     }
+  },
+  watch: {
+    selectedUsers(selectedUsersId) {
+      this.$emit("input", selectedUsersId);
+    }
+  },
+  computed: {
+    preparedDivisions() {
+      let divisionWithUsers = [];
+      this.divisions.forEach(division => {
+        division.id = division.id + "d";
+        divisionWithUsers.push(division);
+        if (division.users.length) {
+          division.users.forEach(user => {
+            divisionWithUsers.push(user);
+          });
+        }
+      });
+      return divisionWithUsers;
+    }
+  }
 };
 </script>
