ec3d43af76795cd2ce90792550d5b41928fa9cd0
Abdulaziz Nurov <nurovaziz@gmail.com>
2020-05-01 13:07:45 +0500

    Make some improvments in timesets view

--- resources/js/components/LeftDrawer.vue
+++ resources/js/components/LeftDrawer.vue
@@ -171,7 +171,7 @@ export default {
                     text: "История задач",
                     url: "/timesets",
                     positions: [],
-                    headOf: ["ОРПО"],
+                    headOf: ["ОРПО","ОУПС"],
                     divisions: []
                 }
             ]
