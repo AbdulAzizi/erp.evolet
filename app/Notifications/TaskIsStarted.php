b6cfdf331bcb19dce9b53193be251bd706f0d0a8
Abdulaziz Nurov <nurovaziz@gmail.com>
2020-05-06 11:01:42 +0500

    Fix broadcast bug for task start and closed

--- app/Notifications/TaskIsStarted.php
+++ app/Notifications/TaskIsStarted.php
@@ -54,6 +54,7 @@ class TaskIsStarted extends Notification
             'avatar' => $this->task->responsible->img,
             'title' => '<a href="' . route("users.dashboard", $this->task->responsible->id) . '">' . $this->task->responsible->full_name . '</a>
                         начал(а) выполнять вашу задачу <a href="' . route("tasks.show", $this->task->id) . '">' . mb_strimwidth($this->task->description, 0, 40, "...") . '</a>',
+            'notification' => $notifiable->notifications()->find($this->id),
         ]);
     }
 }
