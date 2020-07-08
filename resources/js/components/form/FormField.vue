f9f96516514d5917d1f8a483bafcd013e2cfb248
Abdulaziz Nurov <nurovaziz@gmail.com>
2020-07-08 16:17:36 +0500

    Fix form field rounded parameter

--- resources/js/components/form/FormField.vue
+++ resources/js/components/form/FormField.vue
@@ -1,377 +1,372 @@
 <template>
-    <div>
-        <component
-            :is="formField.component"
-            v-bind="{...formField.props}"
-            v-model="fieldVar"
-            ref="field"
-        >
-            <slot v-for="slot in Object.keys($slots)" :name="slot" :slot="slot" />
+  <div>
+    <component
+      :is="formField.component"
+      v-bind="{...formField.props}"
+      v-model="fieldVar"
+      ref="field"
+    >
+      <slot v-for="slot in Object.keys($slots)" :name="slot" :slot="slot" />
 
-            <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope">
-                <slot :name="slot" v-bind="scope" />
-            </template>
-        </component>
-        <input
-            type="hidden"
-            v-if="formField.hasSeparateInput"
-            :name="formField.props.name"
-            :value="JSON.stringify(fieldVar)"
-        />
-    </div>
+      <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope">
+        <slot :name="slot" v-bind="scope" />
+      </template>
+    </component>
+    <input
+      type="hidden"
+      v-if="formField.hasSeparateInput"
+      :name="formField.props.name"
+      :value="JSON.stringify(fieldVar)"
+    />
+  </div>
 </template>
 
 <script>
 import predefinedRules from "./predefinedRules";
 
 const getRuleFunctions = rules => {
-    if (!rules) return;
-    if (!Array.isArray(rules)) return;
+  if (!rules) return;
+  if (!Array.isArray(rules)) return;
 
-    let ruleFunctions = [];
+  let ruleFunctions = [];
 
-    for (const rule of rules) {
-        if (typeof rule !== "string") {
-            ruleFunctions.push(rule);
-            continue;
-        }
-        const predefinedFunctions = predefinedRules.map(pRule =>
-            pRule.name === rule ? pRule.func : true
-        );
-        ruleFunctions = [...ruleFunctions, ...predefinedFunctions];
+  for (const rule of rules) {
+    if (typeof rule !== "string") {
+      ruleFunctions.push(rule);
+      continue;
     }
-    return ruleFunctions;
+    const predefinedFunctions = predefinedRules.map(pRule =>
+      pRule.name === rule ? pRule.func : true
+    );
+    ruleFunctions = [...ruleFunctions, ...predefinedFunctions];
+  }
+  return ruleFunctions;
 };
 
 const getBaseInput = field => {
-    return {
-        component: "input",
-        props: {
-            ...field,
-            name: field.name,
-            value: field.value,
-            type: "hidden"
-        }
-    };
+  return {
+    component: "input",
+    props: {
+      ...field,
+      name: field.name,
+      value: field.value,
+      type: "hidden"
+    }
+  };
 };
 
 const getBaseField = field => {
-    const baseInput = getBaseInput(field);
-    delete baseInput.props.value; //Remove value because there is model fieldVar, and I will assign value to that model
-    delete baseInput.props.type; //Remove type because other fields shoudn't be hidden
+  const baseInput = getBaseInput(field);
+  delete baseInput.props.value; //Remove value because there is model fieldVar, and I will assign value to that model
+  delete baseInput.props.type; //Remove type because other fields shoudn't be hidden
 
-    //Default props for number field
-    baseInput.component = "v-text-field";
-    baseInput.props = {
-        ...baseInput.props,
+  //Default props for number field
+  baseInput.component = "v-text-field";
+  baseInput.props = {
+    ...baseInput.props,
 
-        label: field.label,
-        rules: getRuleFunctions(field.rules),
-        "prepend-icon": field.icon,
-        hint: field.hint,
-        filled: true,
-        rounded: true
-    };
+    label: field.label,
+    rules: getRuleFunctions(field.rules),
+    "prepend-icon": field.icon,
+    hint: field.hint,
+    filled: true,
+    rounded: "rounded" in field ? field.rounded : true
+  };
 
-    return baseInput;
+  return baseInput;
 };
 
 const getStringField = field => getBaseField(field);
 
 const getNumberField = field => {
-    const baseField = getBaseField(field);
+  const baseField = getBaseField(field);
 
-    //Default props for number field
-    baseField.props["type"] = "number";
-    baseField.props["min"] = field.min || 1;
+  //Default props for number field
+  baseField.props["type"] = "number";
+  baseField.props["min"] = field.min || 1;
 
-    return baseField;
+  return baseField;
 };
 
 const getBooleanField = field => {
-    const baseField = getBaseField(field);
+  const baseField = getBaseField(field);
 
-    // //Default props for textarea
-    baseField.component = "v-switch";
+  // //Default props for textarea
+  baseField.component = "v-switch";
 
-    return baseField;
+  return baseField;
 };
 
 const getTextField = field => {
-    const baseField = getBaseField(field);
+  const baseField = getBaseField(field);
 
-    //Default props for textarea
-    baseField.component = "v-textarea";
-    baseField.props["rows"] = baseField.props["rows"] ? baseField.props["rows"] : 3;
+  //Default props for textarea
+  baseField.component = "v-textarea";
+  baseField.props["rows"] = baseField.props["rows"]
+    ? baseField.props["rows"]
+    : 3;
 
-    return baseField;
+  return baseField;
 };
 
 const getSelectField = field => {
-    const baseField = getBaseField(field);
+  const baseField = getBaseField(field);
 
-    //Default props for select field
-    baseField.component = "v-select";
-    baseField.props = {
-        ...baseField.props,
+  //Default props for select field
+  baseField.component = "v-select";
+  baseField.props = {
+    ...baseField.props,
 
-        items: field.items,
-        "item-text": field.itemText ? field.itemText : "name",
-        "item-value": "id"
-    };
+    items: field.items,
+    "item-text": field.itemText ? field.itemText : "name",
+    "item-value": "id"
+  };
 
-    baseField["hasSeparateInput"] = true;
-    return baseField;
+  baseField["hasSeparateInput"] = true;
+  return baseField;
 };
 
 const getAutoCompleteField = field => {
-    const baseSelectField = getSelectField(field);
+  const baseSelectField = getSelectField(field);
 
-    //Default props for autocomplete field
-    baseSelectField.component = "autocomplete";
-    baseSelectField.props = {
-        ...baseSelectField.props,
+  //Default props for autocomplete field
+  baseSelectField.component = "autocomplete";
+  baseSelectField.props = {
+    ...baseSelectField.props,
 
-        color: "primary",
-        multiple: field.multiple ? true : false,
-        hint: field.hint,
-        "persistent-hint": true,
-        "no-data-text": "Данные отсутствуют",
-        "hide-selected": true,
-        "return-object": field.returnObject ? true : false,
-        
-    };
+    color: "primary",
+    multiple: field.multiple ? true : false,
+    hint: field.hint,
+    "persistent-hint": true,
+    "no-data-text": "Данные отсутствуют",
+    "hide-selected": true,
+    "return-object": field.returnObject ? true : false
+  };
 
-    return baseSelectField;
+  return baseSelectField;
 };
 
 const getComboboxField = field => {
-    const baseAutoComplete = getAutoCompleteField(field);
+  const baseAutoComplete = getAutoCompleteField(field);
 
-    //Default props for combobox
-    baseAutoComplete.component = "combobox";
+  //Default props for combobox
+  baseAutoComplete.component = "combobox";
 
-    baseAutoComplete["hasSeparateInput"] = false;
-    return baseAutoComplete;
+  baseAutoComplete["hasSeparateInput"] = false;
+  return baseAutoComplete;
 };
 
 const getManyToManyField = field => {
-    const baseAutoComplete = getAutoCompleteField(field);
+  const baseAutoComplete = getAutoCompleteField(field);
 
-    //Default props for userselector
-    baseAutoComplete.component = "many-to-many-select";
-    baseAutoComplete.props = {
-        ...baseAutoComplete.props,
-        listName: field.listName,
-        relatedListName: field.relatedListName
-    };
+  //Default props for userselector
+  baseAutoComplete.component = "many-to-many-select";
+  baseAutoComplete.props = {
+    ...baseAutoComplete.props,
+    listName: field.listName,
+    relatedListName: field.relatedListName
+  };
 
-    baseAutoComplete["hasSeparateInput"] = false;
-    return baseAutoComplete;
+  baseAutoComplete["hasSeparateInput"] = false;
+  return baseAutoComplete;
 };
 
 const getUsersField = field => {
-    const baseField = getBaseField(field);
+  const baseField = getBaseField(field);
 
-    //Default props for userselector
-    baseField.component = "user-selector";
+  //Default props for userselector
+  baseField.component = "user-selector";
 
-    baseField.props = {
-        ...baseField.props,
-        multiple: field.multiple,
-        icon: field.icon
-    };
-    if (field.users) {
-        baseField.props["users"] = field.users;
-    }
-    if (field.divisions) {
-        baseField.props["divisions"] = field.divisions;
-    }
+  baseField.props = {
+    ...baseField.props,
+    multiple: field.multiple,
+    icon: field.icon
+  };
+  if (field.users) {
+    baseField.props["users"] = field.users;
+  }
+  if (field.divisions) {
+    baseField.props["divisions"] = field.divisions;
+  }
 
-    return baseField;
+  return baseField;
 };
 
 const getPicker = field => {
-    const baseField = getBaseField(field);
+  const baseField = getBaseField(field);
 
-    baseField.component = "picker";
-    baseField.props = {
-        ...baseField.props,
+  baseField.component = "picker";
+  baseField.props = {
+    ...baseField.props,
 
-        pickerType: field.type,
+    pickerType: field.type,
 
-        picker: {
-            min: field.min,
-            max: field.max
-        }
-    };
+    picker: {
+      min: field.min,
+      max: field.max
+    }
+  };
 
-    return baseField;
+  return baseField;
 };
 
 const getYearField = field => {
-    const baseField = getBaseField(field);
+  const baseField = getBaseField(field);
 
-    let years = [];
-    let currentYear = new Date().getFullYear();
+  let years = [];
+  let currentYear = new Date().getFullYear();
 
-    for (let i = 1990; i <= currentYear; i++) {
-        years.push(i);
-    }
+  for (let i = 1990; i <= currentYear; i++) {
+    years.push(i);
+  }
 
-    //Default props for select field
-    baseField.component = "v-select";
-    baseField.props = {
-        ...baseField.props,
+  //Default props for select field
+  baseField.component = "v-select";
+  baseField.props = {
+    ...baseField.props,
 
-        items: years.reverse()
-    };
+    items: years.reverse()
+  };
 
-    baseField["hasSeparateInput"] = true;
-    return baseField;
+  baseField["hasSeparateInput"] = true;
+  return baseField;
 };
 
 const getImagePicker = field => {
-    return {
-        ...field,
-        component: "v-file-input",
-        props: {
-            name: field.name,
-            multiple: true,
-            filled: true,
-            rounded: true,
-            label: field.label,
-            "prepend-icon": "mdi-camera"
-        }
-    };
+  return {
+    ...field,
+    component: "v-file-input",
+    props: {
+      name: field.name,
+      multiple: true,
+      filled: true,
+      rounded: true,
+      label: field.label,
+      "prepend-icon": "mdi-camera"
+    }
+  };
 };
 
 const getEmailField = field => {
-    return {
-        ...field,
-        component: "v-text-field",
-        props: {
-            name: field.name,
-            filled: true,
-            rounded: true,
-            rules: getRuleFunctions(field.rules),
-            error: field.error,
-            "error-messages": field.errorMsg,
-            label: field.label
-        }
-    };
+  return {
+    ...field,
+    component: "v-text-field",
+    props: {
+      name: field.name,
+      filled: true,
+      rounded: true,
+      rules: getRuleFunctions(field.rules),
+      error: field.error,
+      "error-messages": field.errorMsg,
+      label: field.label
+    }
+  };
 };
 
 export default {
-    props: {
-        field: Object,
-        errors: Object | Array,
-        oldInputs: Object | Array,
-        value: null
-    },
+  props: {
+    field: Object,
+    errors: Object | Array,
+    oldInputs: Object | Array,
+    value: null
+  },
 
-    data() {
-        return {
-            fieldVar: this.field.hasOwnProperty("value")
-                ? this.field.value
-                : null
-        };
-    },
-    created() {
-        const fieldHasOldInputs =
-            this.oldInputs && this.oldInputs[this.formField.props.name];
+  data() {
+    return {
+      fieldVar: this.field.hasOwnProperty("value") ? this.field.value : null
+    };
+  },
+  created() {
+    const fieldHasOldInputs =
+      this.oldInputs && this.oldInputs[this.formField.props.name];
 
-        if (fieldHasOldInputs) {
-            this.fieldVar = this.oldInputs[this.formField.props.name]; //FIXME Fix old inputs for multiple fields
-        }
-    },
-    // mounted() {
-    //     console.log(this.formField);
-    // },
-    computed: {
-        formField() {
-            let fieldData = null;
-
-            switch (this.field.type) {
-                case "input":
-                    fieldData = getBaseInput(this.field);
-                    break;
-                case "string":
-                    fieldData = getStringField(this.field);
-                    break;
-                case "text":
-                    fieldData = getTextField(this.field);
-                    break;
-                case "number":
-                    fieldData = getNumberField(this.field);
-                    break;
-                case "boolean":
-                    fieldData = getBooleanField(this.field);
-                    break;
-                case "select":
-                    fieldData = getSelectField(this.field);
-                    break;
-                case "autocomplete":
-                    fieldData = getAutoCompleteField(this.field);
-                    break;
-                case "many-to-many-list":
-                    fieldData = getManyToManyField(this.field);
-                    break;
-                case "combobox":
-                    fieldData = getComboboxField(this.field);
-                    break;
-                case "users":
-                    fieldData = getUsersField(this.field);
-                    break;
-                case "year":
-                    fieldData = getYearField(this.field);
-                    break;
-                case "date":
-                case "time":
-                case "date-time":
-                    fieldData = getPicker(this.field);
-                    break;
-                case "image":
-                    fieldData = getImagePicker(this.field);
-                    break;
-                case "email":
-                    fieldData = getEmailField(this.field);
-                    break;
-            }
-
-            if (this.field.props) {
-                fieldData.props = { ...fieldData.props, ...this.field.props };
-            }
-
-            const fieldHasErrors =
-                this.errors && this.errors[fieldData.props.name];
-
-            if (fieldHasErrors) {
-                fieldData.props["error-messages"] = this.errors[
-                    fieldData.props.name
-                ];
-            }
-            return fieldData;
-        }
-    },
-    watch: {
-        fieldVar(value) {
-            this.$emit("input", value);
-        },
-        value(v) {
-            this.fieldVar = v;
-        },
-        // change value when field prop is changed
-        field(val) {
-            if (this.field.hasOwnProperty("value"))
-                this.fieldVar = this.field.value;
-        }
-    },
-    methods: {
-        validate(value) {
-            this.$refs.field.validate(value);
-        }
+    if (fieldHasOldInputs) {
+      this.fieldVar = this.oldInputs[this.formField.props.name]; //FIXME Fix old inputs for multiple fields
     }
+  },
+  // mounted() {
+  //     console.log(this.formField);
+  // },
+  computed: {
+    formField() {
+      let fieldData = null;
+
+      switch (this.field.type) {
+        case "input":
+          fieldData = getBaseInput(this.field);
+          break;
+        case "string":
+          fieldData = getStringField(this.field);
+          break;
+        case "text":
+          fieldData = getTextField(this.field);
+          break;
+        case "number":
+          fieldData = getNumberField(this.field);
+          break;
+        case "boolean":
+          fieldData = getBooleanField(this.field);
+          break;
+        case "select":
+          fieldData = getSelectField(this.field);
+          break;
+        case "autocomplete":
+          fieldData = getAutoCompleteField(this.field);
+          break;
+        case "many-to-many-list":
+          fieldData = getManyToManyField(this.field);
+          break;
+        case "combobox":
+          fieldData = getComboboxField(this.field);
+          break;
+        case "users":
+          fieldData = getUsersField(this.field);
+          break;
+        case "year":
+          fieldData = getYearField(this.field);
+          break;
+        case "date":
+        case "time":
+        case "date-time":
+          fieldData = getPicker(this.field);
+          break;
+        case "image":
+          fieldData = getImagePicker(this.field);
+          break;
+        case "email":
+          fieldData = getEmailField(this.field);
+          break;
+      }
+
+      if (this.field.props) {
+        fieldData.props = { ...fieldData.props, ...this.field.props };
+      }
+
+      const fieldHasErrors = this.errors && this.errors[fieldData.props.name];
+
+      if (fieldHasErrors) {
+        fieldData.props["error-messages"] = this.errors[fieldData.props.name];
+      }
+      return fieldData;
+    }
+  },
+  watch: {
+    fieldVar(value) {
+      this.$emit("input", value);
+    },
+    value(v) {
+      this.fieldVar = v;
+    },
+    // change value when field prop is changed
+    field(val) {
+      if (this.field.hasOwnProperty("value")) this.fieldVar = this.field.value;
+    }
+  },
+  methods: {
+    validate(value) {
+      this.$refs.field.validate(value);
+    }
+  }
 };
 </script>
 
