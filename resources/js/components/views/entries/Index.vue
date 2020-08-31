897b2d84e0ac7a5a4ef5e76e696cecf767f1efc7
Abdulaziz Nurov <nurovaziz@gmail.com>
2020-08-31 10:51:26 +0500

    Improve some numbers on entries

--- resources/js/components/views/entries/Index.vue
+++ resources/js/components/views/entries/Index.vue
@@ -10,11 +10,12 @@
           <th class="text-left">Присут.</th>
           <th class="text-left">Опозд.</th>
           <th class="text-left">Овертайм</th>
+          <th class="text-left">Ушел раньше</th>
           <th class="text-left">Коментарий</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(entry,index) in preparedEntries" :key="index">
+        <tr v-for="(entry,index) in preparedEntries" :key="index" :class="((moment(entry.date,'YYYY:MM:DD').day() == 6) || (moment(entry.date,'YYYY:MM:DD').day() == 0)) ? 'grey lighten-4' : '' ">
           <td>{{ entry.user }}</td>
           <td>{{ entry.date }}</td>
           <td
@@ -26,8 +27,13 @@
           <td
             :class="entry.present == 'Unknown' ? 'grey--text text--lighten-1' : '' "
           >{{ entry.present }}</td>
-          <td>{{ entry.late }}</td>
-          <td>{{ entry.overtime }}</td>
+          <td :class="entry.late == 'Unknown' ? 'grey--text text--lighten-1' : '' ">{{ entry.late }}</td>
+          <td
+            :class="entry.overtime == 'Unknown' ? 'grey--text text--lighten-1' : '' "
+          >{{ entry.overtime }}</td>
+          <td
+            :class="entry.early == 'Unknown' ? 'grey--text text--lighten-1' : '' "
+          >{{ entry.early }}</td>
           <v-hover v-slot:default="{ hover }">
             <td style="position:relative;">
               <v-text-field
@@ -66,6 +72,9 @@ export default {
   data() {
     return {
       preparedEntries: [],
+      workDays:0,
+      timeToLeave: null,
+      hoursPerDay: null
     };
   },
   created() {
@@ -90,47 +99,126 @@ export default {
           Event.fire("notify", ["Сохранено успешно!"]);
         });
     },
+    msToTime(duration) {
+      var milliseconds = parseInt((duration % 1000) / 100),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) );
+
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      return hours + ":" + minutes;
+    },
     prepareEntries() {
+      let totalPresent = this.moment.duration("00:00:00");
+      let totalLate = this.moment.duration("00:00:00");
+      let totalOvertime = this.moment.duration("00:00:00");
+      let totalEarly = this.moment.duration("00:00:00");
+
       this.entries.forEach((e) => {
         let entry = { ...e };
+        if (e.user.position_level_id == 1) {
+          this.hoursPerDay = 11;
+          this.timeToLeave = "19:30:00";
+        } else {
+          this.hoursPerDay = 9;
+          this.timeToLeave = "17:30:00";
+        }
         entry["user"] = e.user ? e.user.name + " " + e.user.surname : "Unknown";
         entry["date"] = e.date;
-        entry["sign_in"] = e.sign_in ? e.sign_in : "Unknown";
-        entry["sign_out"] = e.sign_out ? e.sign_out : "Unknown";
-        // ----------------------------------------------
+        if(((this.moment(e.date,'YYYY:MM:DD').day() != 6) && (this.moment(e.date,'YYYY:MM:DD').day() != 0)))
+        {
+          this.workDays++;
+        }
+        if (e.sign_in) {
+          if (e.sign_in > "08:30") {
+            entry["late"] = this.moment(
+              this.moment(e.sign_in, "HH:mm").diff(
+                this.moment("08:30", "HH:mm")
+              )
+            ).format("HH:mm:ss");
+          } else {
+            entry["late"] = "00:00:00";
+          }
+        } else {
+          entry["sign_in"] = "Unknown";
+          entry["late"] = "Unknown";
+        }
+        if (e.sign_out) {
+          if (this.timeToLeave > e.sign_out) {
+            entry["early"] = this.moment(
+              this.moment(this.timeToLeave, "HH:mm:ss").diff(
+                this.moment(entry.sign_out, "HH:mm:ss")
+              )
+            ).format("HH:mm:ss");
+          } else {
+            entry["early"] = "00:00:00";
+          }
+        } else {
+          entry["sign_out"] = "Unknown";
+          entry["early"] = "Unknown";
+        }
         if (e.sign_in && e.sign_out) {
           entry["present"] = this.moment(
-            this.moment(entry.sign_out, "HH:mm").diff(
-              this.moment(entry.sign_in, "HH:mm")
+            this.moment(e.sign_out, "HH:mm").diff(
+              this.moment(e.sign_in, "HH:mm")
             )
           ).format("HH:mm:ss");
+          // ----------------------------------------------
+
+          if (entry["present"] > this.moment(this.hoursPerDay, "H").format("HH:mm:ss")) {
+            entry["overtime"] = this.moment(
+              this.moment(entry["present"], "HH:mm:ss") -
+                this.moment(this.hoursPerDay, "H")
+            ).format("HH:mm:ss");
+          } else {
+            entry["overtime"] = "00:00:00";
+          }
         } else {
           entry["present"] = "Unknown";
+          entry["overtime"] = "Unknown";
         }
-        // ----------------------------------------------
-        if (e.sign_in > "08:30") {
-          entry["late"] = this.moment(
-            this.moment(e.sign_in, "HH:mm").diff(this.moment("08:30", "HH:mm"))
-          ).format("HH:mm:ss");
-        } else {
-          entry["late"] = "00:00:00";
-        }
-        // ----------------------------------------------
-        if (entry["present"] != "Unknown") {
-          entry["overtime"] = this.moment(
-            this.moment(entry["present"], "HH:mm:ss") -
-              this.moment("09:00:00", "HH:mm:ss")
-          ).format("HH:mm:ss");
-        } else {
-          entry["overtime"] = "00:00:00";
-        }
+
         entry["comment"] = {
           model: e.comment,
           text: e.comment,
           editing: false,
         };
 
+        if(entry["present"] != "Unknown"){
+          totalPresent.add( this.moment.duration(entry["present"]) );
+        }
+        if(entry["late"] != "Unknown"){
+          totalLate.add( this.moment.duration(entry["late"]) );
+        }
+        if(entry["overtime"] != "Unknown"){
+          totalOvertime.add( this.moment.duration(entry["overtime"]) );
+        }
+        if(entry["early"] != "Unknown"){
+          totalEarly.add( this.moment.duration(entry["early"]) );
+        }
+
         this.preparedEntries.push(entry);
+
+        // console.log(totalPresent);
+        // console.log(totalPresent.format("HH:mm:ss"));
+        // console.log(totalLate.format("HH:mm:ss"));
+        // console.log(totalOvertime.format("HH:mm:ss"));
+        // console.log(totalEarly.format("HH:mm:ss"));
+      });
+
+      this.preparedEntries.push({
+        user: "",
+        date: "",
+        sign_in: "",
+        sign_out: "",
+        present: this.msToTime(totalPresent) + '/' + (this.hoursPerDay * this.workDays),
+        late: this.msToTime(totalLate),
+        overtime: this.msToTime(totalOvertime),
+        early: this.msToTime(totalEarly),
+        comment: "",
       });
     },
   },
--- resources/views/users/entries.blade.php
+++ resources/views/users/entries.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.profile')
 
 @section('content')
-    <v-container class="py-0">
+    <v-container fluid class="pa-0" style="max-width:1200px;">
         <views-entries-index class="mt-3" :entries="{{$entries}}"></views-entries-index>
     </v-container>
 @endsection 
