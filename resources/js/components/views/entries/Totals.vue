a662f13fe0367e0806104d642aad2d4feb5def26
Abdulaziz Nurov <nurovaziz@gmail.com>
2020-09-08 08:55:49 +0500

    Add more details to entry totals

--- resources/js/components/views/entries/Totals.vue
+++ resources/js/components/views/entries/Totals.vue
@@ -4,21 +4,35 @@
       <thead>
         <tr>
           <th class="text-left">Сотрудник</th>
-          <th class="text-left">Присут.</th>
-          <th class="text-left">Присут %.</th>
-          <th class="text-left">Опозд.</th>
+          <th class="text-left">Рабочие дни в месяц</th>
+          <th class="text-left">Рабочие часы в день</th>
+          <th class="text-left">Рабочие часы в месяц</th>
+          <th class="text-left">Присутствовал месяц часы</th>
+          <th class="text-left">Опоздал на работу</th>
+          <th class="text-left">Ушел с работы пораньше</th>
+          <th class="text-left">Присутствовал на работе</th>
+          <th class="text-left">Не отработанные дни</th>
+          <th class="text-left">Не отработанные часы</th>
           <th class="text-left">Овертайм</th>
-          <th class="text-left">Ушел раньше</th>
+          <th class="text-left">Количество опозданий в месяц</th>
+          <th class="text-left">Присутствовал в %.</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(user,index) in localUsers" :key="index">
           <td>{{ user.name }}</td>
+          <td>{{ user.workDays }}</td>
+          <td>{{ user.hoursPerDay+':00:00' }}</td>
+          <td>{{ (user.workDays * user.hoursPerDay)+':00:00' }}</td>
           <td>{{ msToTime(user.totalPresent) }}</td>
-          <td>{{Number.isNaN(user.presentPercentage) ? 0 : user.presentPercentage}}%</td>
           <td>{{ msToTime(user.totalLate) }}</td>
-          <td>{{ msToTime(user.totalOvertime) }}</td>
           <td>{{ msToTime(user.totalEarly) }}</td>
+          <td>{{ user.presentDays }}</td>
+          <td>{{ user.workDays - user.presentDays}}</td>
+          <td>{{ ((user.workDays - user.presentDays) * user.hoursPerDay)+':00:00' }}</td>
+          <td>{{ msToTime(user.totalOvertime) }}</td>
+          <td>{{ user.totalLateDays }}</td>
+          <td>{{Number.isNaN(user.presentPercentage) ? 0 : user.presentPercentage}}%</td>
         </tr>
       </tbody>
     </template>
@@ -34,9 +48,6 @@ export default {
   data() {
     return {
       localUsers: [],
-      workDays: 0,
-      hoursPerDay: 0,
-      timeToLeave: null,
     };
   },
   created() {
@@ -45,8 +56,6 @@ export default {
   methods: {
     prepareEntries() {
       this.users.forEach((u) => {
-        this.workDays = 0;
-
         let user = {
           name: u.name + " " + u.surname,
           totalPresent: this.moment.duration(),
@@ -54,15 +63,20 @@ export default {
           totalLate: this.moment.duration(),
           totalOvertime: this.moment.duration(),
           totalEarly: this.moment.duration(),
+          totalLateDays: 0,
+          workDays: 0,
+          hoursPerDay: 0,
+          presentDays: 0,
+          timeToLeave: null,
         };
 
         u.entries.forEach((e) => {
           if (u.position_level_id == 1) {
-            this.hoursPerDay = 11;
-            this.timeToLeave = "19:30:00";
+            user.hoursPerDay = 11;
+            user.timeToLeave = "19:30:00";
           } else {
-            this.hoursPerDay = 9;
-            this.timeToLeave = "17:30:00";
+            user.hoursPerDay = 9;
+            user.timeToLeave = "17:30:00";
           }
 
           if (e.sign_in && e.sign_out) {
@@ -71,6 +85,7 @@ export default {
                 this.moment(e.sign_in, "HH:mm")
               )
             );
+            user.presentDays++;
           }
 
           if (e.sign_in > "08:30") {
@@ -79,6 +94,7 @@ export default {
                 this.moment("08:30", "HH:mm")
               )
             );
+            user.totalLateDays++;
           }
 
           let present = this.moment(
@@ -89,31 +105,34 @@ export default {
 
           if (
             present.format("HH:mm:ss") >
-            this.moment(this.hoursPerDay, "H").format("HH:mm:ss")
+            this.moment(user.hoursPerDay, "H").format("HH:mm:ss")
           ) {
             user.totalOvertime.add(
-              present.subtract(this.hoursPerDay, "hours").format("HH:mm:ss")
+              present.subtract(user.hoursPerDay, "hours").format("HH:mm:ss")
             );
           }
 
-          if (this.timeToLeave > e.sign_out) {
+          if (user.timeToLeave > e.sign_out) {
             user.totalEarly.add(
-              this.moment(this.timeToLeave, "HH:mm:ss").diff(
+              this.moment(user.timeToLeave, "HH:mm:ss").diff(
                 this.moment(e.sign_out, "HH:mm:ss")
               )
             );
           }
 
           if (
-            this.moment(e.date, "YYYY:MM:DD").day() != 6 &&
-            this.moment(e.date, "YYYY:MM:DD").day() != 0
+            (this.moment(e.date, "YYYY:MM:DD").day() != 0 &&
+              u.position_level_id == 1) ||
+            (u.position_level_id != 1 &&
+              this.moment(e.date, "YYYY:MM:DD").day() != 6 &&
+              this.moment(e.date, "YYYY:MM:DD").day() != 0)
           ) {
-            this.workDays++;
+            user.workDays++;
           }
         });
         user.presentPercentage = Math.round(
           (this.moment.duration(user.totalPresent).asHours() * 100) /
-            (this.workDays * this.hoursPerDay)
+            (user.workDays * user.hoursPerDay)
         );
 
         this.localUsers.push(user);
