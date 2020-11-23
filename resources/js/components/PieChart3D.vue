9676e7d46c1d152d7a39653689d255260f66f2dc
Abdulaziz Nurov <nurovaziz@gmail.com>
2020-11-23 11:49:34 +0500

    Add graph timesets Report

---
+++ resources/js/components/PieChart3D.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="hello" ref="chartdiv"></div>
+</template>
+<script>
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
+
+export default {
+    props:{
+        data:{
+            required: true
+        }
+    },
+    mounted() {
+        let chart = am4core.create(this.$refs.chartdiv, am4charts.PieChart3D);
+        chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+        chart.legend = new am4charts.Legend();
+        chart.data = this.data;
+        chart.innerRadius = 100;
+        let series = chart.series.push(new am4charts.PieSeries3D());
+        series.dataFields.value = "value";
+        series.dataFields.category = "text";
+    }
+};
+</script>
+<style>
+.hello {
+    width: 100%;
+    height: 500px;
+}
+</style>
